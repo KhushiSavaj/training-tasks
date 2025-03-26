e6391de5b816eaa19b9e47d453b11fa06c1fe523
Khushi Savaj <savajkhushi04@gmail.com>
2025-03-26 16:48:12 +0530

    - initial commit

---
+++ src/components/tasks/index.tsx
@@ -0,0 +1,26 @@
+import { tasks } from "../../constant";
+export interface ITasks {
+  id: number;
+  title: string;
+  description: string;
+}
+
+const Tasks = () => {
+  return (
+    <div className="home-content container">
+      {tasks.map((res) => {
+        return (
+          <div className="card">
+            <div className="card-heading">
+              <div>{res.id}</div>
+              <div>{res.title}</div>
+            </div>
+            <div className="card-description">{res.description}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Tasks;
---
+++ src/constant/index.tsx
@@ -0,0 +1,54 @@
+import { ITasks } from "../components/tasks";
+
+export const tasks: ITasks[] = [
+  {
+    id: 1,
+    title: "Counter",
+    description:"Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
+  },
+  {
+    id: 2,
+    title: "Calculator",
+    description:"Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
+  },
+  {
+    id: 3,
+    title: "Counter",
+    description:"Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
+  },
+  {
+    id: 4,
+    title: "Calculator",
+    description:"Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
+  },
+  {
+    id: 5,
+    title: "Counter",
+    description:"Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
+  },
+  {
+    id: 6,
+    title: "Calculator",
+    description:"Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
+  },
+  {
+    id: 7,
+    title: "Counter",
+    description:"Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
+  },
+  {
+    id: 8,
+    title: "Calculator",
+    description:"Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
+  },
+  {
+    id: 9,
+    title: "Counter",
+    description:"Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
+  },
+  {
+    id: 10,
+    title: "Calculator",
+    description:"Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
+  },
+];
