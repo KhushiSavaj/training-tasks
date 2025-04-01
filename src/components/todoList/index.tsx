7f1fc5b10eca07c613fa59becab367b6e4331296
Khushi Savaj <savajkhushi04@gmail.com>
2025-04-01 14:51:16 +0530

    - implement todo list

---
+++ src/components/todoList/index.tsx
@@ -0,0 +1,95 @@
+import ArrowBackOutlinedIcon from "@mui/icons-material/ArrowBackOutlined";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import { useState } from "react";
+import styles from "./TodoList.module.scss";
+import { useNavigate } from "react-router-dom";
+
+interface ITask {
+  id: number;
+  name: string;
+}
+
+const TodoList = () => {
+  const [tasks, setTasks] = useState<ITask[]>([]);
+  const [inputValue, setInputValue] = useState("");
+  const [selectedTask, setSelectedTask] = useState<ITask | null>(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    if (selectedTask) {
+      const updateValue = tasks.map((res) => {
+        if (selectedTask?.id === res?.id) {
+          return { ...res, name: inputValue };
+        }
+        return res;
+      });
+      setTasks(updateValue);
+      setSelectedTask(null);
+    } else {
+      setTasks([...tasks, { id: Math.random(), name: inputValue }]);
+    }
+    setInputValue("");
+  };
+
+  const handleUpdate = (task: ITask) => {
+    setSelectedTask(task);
+    setInputValue(task.name);
+  };
+
+  const handleDelete = (index: number) => {
+    const newTodos = [...tasks];
+    newTodos.splice(index, 1);
+    setTasks(newTodos);
+  };
+
+  return (
+    <div>
+      <div onClick={() => navigate("/")} className={styles.backToTask}>
+        <ArrowBackOutlinedIcon />
+        <div>{`Back To Task`}</div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.taskDetails}>
+          <div className={styles.taskName}>{`4. Todo List`}</div>
+          <div className={styles.taskDescription}>
+            {`Build a simple todo list where users can add and remove tasks using state and list rendering.`}
+          </div>
+        </div>
+        <div className={styles.taskSection}>
+          <label className={styles.label}>{`Add a new task :`}</label>
+          <div className={styles.fieldGrid}>
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter a new task"
+              className={styles.input}
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button onClick={handleSubmit} className={styles.addButton}>
+              {selectedTask !== null ? `EDIT` : `ADD`}
+            </button>
+          </div>
+        </div>
+
+        <div className={styles.taskList}>
+          <div className={styles.taskId}>{`Tasks (${tasks?.length})`}</div>
+          {tasks.map((res, index) => {
+            return (
+              <div className={styles.todosList}>
+                <div className={styles.listName}>{res.name}</div>
+                <div className={styles.gridButton}>
+                  <EditOutlinedIcon onClick={() => handleUpdate(res)} />
+                  <DeleteOutlinedIcon onClick={() => handleDelete(index)} />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
